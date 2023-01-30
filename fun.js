f08cb5b8fd5aa34b29d1a0ad0b0a33f905f501cb
Cristian Vaca <civaca7@gmail.com>
2023-01-30 16:18:33 -0500

    final changes

--- fun.js
+++ fun.js
@@ -1,6 +1,19 @@
 const {useState,useEffect}=React
 
+marked.setOptions({
+    gfm:true,
+    breaks:true
 
+});
+const intialMark="# Welcome\n## To Markdown Preview\n"+
+"## Write your markdown Code to render like so:\n"+
+"There's also [links](https://www.freecodecamp.org)\n"+ 
+"    This a code block\n\n    return"+
+ "\nHeres some code, `<div></div>`, between 2 backticks."+
+  "\n![freeCodeCamp Logo](https://www.multidots.com/wp-content/uploads/2020/01/code-quality-standard.png?quality=90)"+
+   "\nYou can also make text **bold**... whoa!"+
+    "- And of course there are lists."+ 
+    "\n - First item \n1. Hola\n"+ " > Block Quotes!"
 let MARKDOWN=()=>{
 const[text,setText]=useState("de");
 useEffect(()=>handleClick(),[]);
@@ -16,8 +29,11 @@ let handleClick=()=>{
    
 return(
     <div>
-    <textarea id ="editor"onChange={handleClick}># TExt \n ## hola</textarea>
-    <div id="preview"></div>
+        <textarea class="form-control"id ="editor"onChange={handleClick}>{intialMark}</textarea>
+        <div class="toolbar btn btn-block btn-success">
+            <h4>Rendered Elements</h4>
+        </div> 
+        <div id="preview"></div>
     </div>
     )
 };
